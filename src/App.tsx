0f94b0536336cdb9eb2fd41395d7195795c7cd91
unknown <carolinascarnevali@gmail.com>
2023-10-03 17:55:03 -0300

    pagina login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-//import { useState } from 'react'
-import './App.css'
-
-import Home from "./pagina/home/Home"
-
+import React from 'react';
+import './App.css';
+import Home from './pagina/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pagina/home/login/Login';
 function App() {
-
   return (
-    <Home />
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
-
-export default App
+export default App;
