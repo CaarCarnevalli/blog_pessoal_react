2d1e938ec625c524cca8eee42094b9baed37bbf0
unknown <carolinascarnevali@gmail.com>
2023-10-09 12:50:38 -0300

    Desenvolvendo crud tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import './App.css';
-import Home from './pagina/home/Home';
+
+import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pagina/home/login/Login';
+import Cadastro from './pagina/cadastro/Cadastro';
+import Home from './pagina/home/Home';
+import { AuthProvider } from './pagina/home/contexts/AuthContext';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>
     </>
   );
 }
